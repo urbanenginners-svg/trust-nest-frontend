52ecc708efac6a515a83e68729b2f587952ddb48
RajanBakshi238 <rajan.bakshi238@gmail.com>
2026-02-01 20:48:28 +0530

    WIP: tanstack query implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { RouterProvider } from "react-router-dom";
+
 import router from "./routes/router";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 5 * 60 * 1000,
+            retry: 1,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 const App = () => {
 
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
-        </>
+        </QueryClientProvider>
     );
 };
 export default App;
